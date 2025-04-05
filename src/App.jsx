46d480d5135db36683d4469a39fd743ab5a5b486
suraj savle <surajsavle143@gmail.com>
2025-04-05 22:31:26 +0530

    login authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import './index.css'
-import Home from "./Pages/Home"
-
+import React from "react";
+import "./index.css";
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import CreateResume from "./Pages/CreateResume";
+import SignUp from "./Pages/SignUp";
+import UpdateResume from "./Pages/UpdateResume";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 
 function App() {
+  const { user, isSignedIn } = useUser();
+
+  if (isSignedIn) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
-    <Home />
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={user ? <Dashboard /> : <SignUp />} />
+        <Route path="/dashboard" element={isSignedIn ? <Dashboard /> : <Navigate to="/sign-in" />} />
+        <Route path="/createresume" element={isSignedIn ? <CreateResume /> : <Navigate to="/sign-in" />} />
+        <Route path="/updateResume" element={isSignedIn ? <UpdateResume /> : <Navigate to="/sign-in" />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/clerk-react'
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='flex ' >
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignUp
